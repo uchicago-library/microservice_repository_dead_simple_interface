bbbbec7fe8cff46b6db69825517594cbd862ac51
brian <balsamo@uchicago.edu>
2017-05-02 17:13:08 -0500

    removing the config from the repo

--- config.py
+++
@@ -1,4 +0,0 @@
-INTERNAL_ACC_IDNEST_URL="http://acc_idnest:8083/"
-EXTERNAL_ARCHSTOR_URL="http://localhost:8080/archstor/"
-EXTERNAL_QREMIS_API_URL="http://localhost:8080/qremis_api/"
-INTERNAL_QREMIS_API_URL="http://qremis_api:8082/"
